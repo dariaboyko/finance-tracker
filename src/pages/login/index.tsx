3cd9f4ffdd5844ee1de4f8da70847fe4d843f759
dariaboyko <dariaboiko03@gmail.com>
2024-02-12 23:55:54 +0200

    init login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-
+import { MailTwoTone, LockTwoTone } from '@ant-design/icons';
+import { Input } from 'antd';
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login
+      <Input placeholder="Email" type="email" prefix={<MailTwoTone />} />
+      <Input placeholder="Password" type="password" prefix={<LockTwoTone />} />
+    </div>
+  );
 };
 
 export default Login;
