28ca1e09101dae58276f530e90dadfa73b29e565
Oleksandr Panchenko <Panchenko.Sasha.nick74@gmail.com>
2024-05-09 22:20:33 +0300

    added role

--- src/utils/tokenService.tsx
+++ src/utils/tokenService.tsx
@@ -5,6 +5,7 @@ const REFRESH_TOKEN_KEY = 'refresh_token';
 const USER_ID_KEY = 'user_id';
 const USER_NAME_KEY = 'user_name';
 const USER_EMAIL_KEY = 'user_email';
+const USER_ROLE_KEY = 'user_role';
 
 export const setToken = (token: string): void => {
   Cookies.set(TOKEN_KEY, token, { expires: 1, path: '/' });
@@ -48,6 +49,14 @@ export const setUserEmail = (refreshToken: string): void => {
 
 export const getUserEmail = (): string | undefined => {
   return Cookies.get(USER_EMAIL_KEY);
+};
+
+export const setUserRole = (role: string): undefined => {
+  Cookies.set(USER_ROLE_KEY, role);
+};
+
+export const getUserRole = (): string | undefined => {
+  return Cookies.get(USER_ROLE_KEY);
 };
 
 export const removeRefreshToken = (): void => {
